a447fbf11a657ffe9374ad91a81c04fb0e9a7d49
iman <iman@bitmascot.com>
2017-04-25 17:19:18 +0600

    app

--- nodewebapp/routes/products.js
+++ nodewebapp/routes/products.js
@@ -22,7 +22,7 @@ router.route('/')
     //GET all blobs
     .get(function(req, res, next) {
         //retrieve all blobs from Monogo
-        mongoose.model('Blob').find({}, function (err, blobs) {
+        mongoose.model('Product').find({}, function (err, blobs) {
             if (err) {
                 return console.error(err);
             } else {
@@ -30,8 +30,8 @@ router.route('/')
                 res.format({
                     //HTML response will render the index.jade file in the views/blobs folder. We are also setting "blobs" to be an accessible variable in our jade view
                     html: function(){
-                        res.render('blobs/index', {
-                            title: 'All my Blobs',
+                        res.render('products/index', {
+                            title: 'All Products',
                             "blobs" : blobs
                         });
                     },
@@ -43,6 +43,44 @@ router.route('/')
             }
         });
     })
+    //POST a new blob
+    .post(function(req, res) {
+        console.log(req);
+        // Get values from POST request. These can be done through forms or REST calls. These rely on the "name" attributes for forms
+        /*var name = req.body.name;
+        var badge = req.body.badge;
+        var dob = req.body.dob;
+        var company = req.body.company;
+        var isloved = req.body.isloved;
+        //call the create function for our database
+        mongoose.model('Blob').create({
+            name : name,
+            badge : badge,
+            dob : dob,
+            isloved : isloved
+        }, function (err, blob) {
+            if (err) {
+                res.send("There was a problem adding the information to the database.");
+            } else {
+                //Blob has been created
+                console.log('POST creating new blob: ' + blob);
+                res.format({
+                    //HTML response will set the location and redirect back to the home page. You could also create a 'success' page if that's your thing
+                    html: function(){
+                        // If it worked, set the header so the address bar doesn't still say /adduser
+                        res.location("blobs");
+                        // And forward to success page
+                        res.redirect("/blobs");
+                    },
+                    //JSON response will show the newly created blob
+                    json: function(){
+                        res.json(blob);
+                    }
+                });
+            }
+        })*/
+    });
+
 
 /* GET New Product page. */
 router.get('/new', function(req, res) {
