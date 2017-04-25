17dd3a245cebf15896d89ec4b181ede8b1f2695c
iman <iman@bitmascot.com>
2017-04-25 13:02:18 +0600

    app

--- db.php
+++ db.php
@@ -1,1 +1,13 @@
+To list all databases:
 show dbs
+
+To enter or use given database:
+use databasename
+
+To list all collections:
+show collections
+show tables
+db.getCollectionNames()
+
+choose your collection and type the following to see all contents of that collection:
+db.collectionName.find()
