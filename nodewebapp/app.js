778f781933c08238838f47fe7bfdaec5d256f103
iman <iman@bitmascot.com>
2017-05-04 10:21:28 +0600

    view as html

--- nodewebapp/app.js
+++ nodewebapp/app.js
@@ -23,8 +23,8 @@ app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
 
 // This part added for HTML file as view
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+//app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
