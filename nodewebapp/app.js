bb5ebb821b6557b23e48b87c4eed2e6d13429073
iman <iman@bitmascot.com>
2017-05-04 15:24:56 +0600

    post detail

--- nodewebapp/app.js
+++ nodewebapp/app.js
@@ -1,58 +1,31 @@
 // require and instantiate express
 const app = require('express')()
 
-// fake posts to simulate a database
-const posts = [
-  {
-    id: 1,
-    author: 'John',
-    title: 'Templating with EJS',
-    body: 'Blog post number 1'
-  },
-  {
-    id: 2,
-    author: 'Drake',
-    title: 'Express: Starting from the Bottom',
-    body: 'Blog post number 2'
-  },
-  {
-    id: 3,
-    author: 'Emma',
-    title: 'Streams',
-    body: 'Blog post number 3'
-  },
-  {
-    id: 4,
-    author: 'Cody',
-    title: 'Events',
-    body: 'Blog post number 4'
-  }
-]
+var routes = require('./routes/index');
+var posts = require('./routes/posts');
 
 // set the view engine to ejs
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-// blog home page
-app.get('/blog', (req, res) => {
-  // render `home.ejs` with the list of posts
-  res.render('posts/index', { posts: posts })
-});
+app.use('/', routes);
+app.use('/posts', posts);
+
 
 // blog post
-app.get('/post/:id', (req, res) => {
+app.get('/post/:id', function(req, res){
   // find the post in the `posts` array
-  const post = posts.filter((post) => {
+  /*const post = posts.filter((post) => {
     return post.id == req.params.id
-  })[0]
+  })[0]*/
 
   // render the `post.ejs` template with the post content
-  res.render('post', {
-    author: post.author,
-    title: post.title,
-    body: post.body
-  })
+    res.render('posts/post', {
+        author: 'a',
+        title: 'b',
+        body: 'ddddd'
+    });
 });
 
 app.listen(3000)
 
-console.log('listening on port 8080')
+console.log('listening on port 3000')
--- nodewebapp/routes/posts.js
+++ nodewebapp/routes/posts.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
+
+
 // fake posts to simulate a database
-const posts = [
+var posts = [
   {
     id: 1,
     author: 'John',
@@ -29,10 +31,43 @@ const posts = [
     body: 'Blog post number 4'
   }
 ]
+
+//console.log(posts);
+
 // blog home page
-app.get('/', (req, res) => {
+router.route('/')
+    //GET all products
+    .get(function(req, res) {
   // render `home.ejs` with the list of posts
-  res.render('home', { posts: posts })
+  res.render('posts/index', { posts: posts })
 });
+
+
+// route middleware to validate :id
+router.param('id', function(req, res, next, id) {
+      req.id = id;
+      next();
+});
+
+// blog post
+router.route('/:id').get(function(req, res){
+ // find the post in the `posts` array
+  console.log(req);
+
+  res.render('posts/post', {
+    author: 'a',
+    title: 'b',
+    body: 'ddddd'
+  });
+
+});
+
+ // render the `post.ejs` template with the post content
+ /* res.render('posts/post', {
+   author: post.author,
+   title: post.title,
+   body: post.body
+   });*/
+
 
 module.exports = router;
