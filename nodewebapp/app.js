6a2378b1858abe02f957aa07b7a5bed91beaef2c
imanali <imanali.cse@gmail.com>
2017-05-10 21:32:30 +0600

    blog image

--- nodewebapp/app.js
+++ nodewebapp/app.js
@@ -1,12 +1,11 @@
 // require and instantiate express
-const app = require('express')()
+var app = require('express')()
 
 var index = require('./routes/index');
 var posts = require('./routes/posts');
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-app.use( express.static( "public" ) );
 
 app.use('/', index);
 app.use('/posts', posts);
